24f0369d0cf03f8437f808c6fe5d61b32c835d1f
Bengilla <bengilla@outlook.com>
2022-01-04 17:43:26 +0800

    Update

--- main.py
+++ main.py
@@ -110,5 +110,9 @@ def form():
             connection.send_message(message)
     return render_template("index.html")
 
+@app.route("/test")
+def test():
+    return render_template("test.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
